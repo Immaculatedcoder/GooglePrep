d49f6be127478bd49a31fc86625aa807280a6718
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-11-25 16:28:46 -0500

    h

---
+++ FCC/TwoPointer/LC876.py
@@ -0,0 +1,19 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def middleNode(head):
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
