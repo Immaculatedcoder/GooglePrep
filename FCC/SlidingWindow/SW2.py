0422cd95b8b6b643af3f19ea2be6b886e2bd60db
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2025-11-26 12:35:44 -0500

    aa

--- FCC/SlidingWindow/SW2.py
+++ FCC/SlidingWindow/SW2.py
@@ -1,2 +1,18 @@
 # Given a string s, find the length of the longest substring without duplicate characters.
 
+def lengthOfLongestSubstring(s):
+    seen = set()
+    l = 0
+    longest = 0
+    for r in range(len(s)):
+        while s[r] in seen:
+            seen.remove(s[l])
+            l += 1
+        
+        seen.add(s[r])
+        w = r - l + 1
+        longest = max(longest, w)
+    return longest
+
+s = "abcabcbb"
+print(lengthOfLongestSubstring(s))
